ebde53d2e8b9cd44a26f4459b1e612f6a1b9ddf9
DANIEL KINDANGO <daniel.kindango@sintaxy.com>
2025-04-29 14:00:17 +0100

    updating Configurations

---
+++ collaborative-platform/src/test/java/kinSoftware/collaborative_platform/CollaborativePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package kinSoftware.collaborative_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborativePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
