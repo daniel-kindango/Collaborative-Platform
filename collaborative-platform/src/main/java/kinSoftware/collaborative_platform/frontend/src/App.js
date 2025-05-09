c7d5520fc7e73f02681a5e76572bd97fe4ad5039
DANIEL KINDANGO <daniel.kindango@sintaxy.com>
2025-05-09 07:24:59 +0100

    FrontEnd Login, Sign Up screen

---
+++ collaborative-platform/src/main/java/kinSoftware/collaborative_platform/frontend/src/App.js
@@ -0,0 +1,20 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Signup from './Signup';
+import ForgotPassword from './ForgotPassword';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
